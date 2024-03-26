4988f5e7e15bf4a89d62294468eb41b11fabb469
YozovaZuhro <z.yorzoda@gmail.com>
2024-03-26 11:23:21 -0700

    loginpage

--- src/layout/menu.tsx
+++ src/layout/menu.tsx
@@ -1,24 +1,61 @@
 import "./menu.css";
 import { Link, useLocation } from "react-router-dom";
+
 function Menu() {
   const location = useLocation();
+  const linkStyle1 = (path:string) => ({
+    color: location.pathname === path ? "#FCA713" : "#0A0A0A",
+  });
+
+  function linkStyle(path: string) {
+    return {
+      color: location.pathname === path ? "#007bff" : "#ffffff",
+      textDecoration: "none",
+    };
+  }
 
   return (
     <div>
-      <ul>
-        <li className="indent">
-          {location.pathname === "/form-filling-page" && (
-            <button className="menu">
-              <Link
-                to="/login-page"
-                style={{ color: "#ffffff", textDecoration: "none" }}
-              >
-                войти
-              </Link>
-            </button>
-          )}
-        </li>
+      <ul className="indent">
+        {location.pathname === "/form-filling-page" && (
+          <button className="menu">
+          <Link
+            to="/login-page"
+            style={{ color: "#ffffff", textDecoration: "none" }}
+          >
+            войти
+          </Link>
+        </button>
+        )}
       </ul>
+      {location.pathname === "/request-page" && (
+        <ul className="indent1">
+          <li className="indent">
+            <Link to="/request-page" style={linkStyle1("/request-page")}>
+              Мои заявки
+            </Link>
+          </li>
+          <li className="indent">
+            <Link to="/credits-page" style={linkStyle1("/credits-page")}>
+              Мои кредиты
+            </Link>
+          </li>
+        </ul>
+      )}
+        {location.pathname === "/credits-page" && (
+        <ul className="indent1">
+          <li className="indent">
+            <Link to="/request-page" style={linkStyle1("/request-page")}>
+              Мои заявки
+            </Link>
+          </li>
+          <li className="indent">
+            <Link to="/credits-page" style={linkStyle1("/credits-page")}>
+              Мои кредиты
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,48 @@
+
+import { useNavigate } from 'react-router-dom'; 
+import "./LoginPage.css";
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate(); 
+
+  const handleClick = () => {
+    navigate('/request-page'); 
+  };
+
+  return (
+    <div className="maincontainer">
+      <div>
+        <p>Вход в систему</p>
+        <span>
+          Пожалуйста, введите ваш логин и пароль <br></br>ниже, чтобы войти в ваш аккаунт.
+        </span>
+      </div>
+      <div className="formcontainer">
+        <form>
+          <label className="label required">Логин</label>
+          <input
+            name="name"
+            type="text"
+            className="input1"
+            placeholder="ivanovivan@mail.com"
+            required
+          />
+
+          <label className="label required">Пароль</label>
+          <input
+            name="password" 
+            type="password" 
+            className="input2"
+            placeholder="*********"
+            required
+          />
+          <button type="button" className="submitbutton" onClick={handleClick}>
+            Войти
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export { LoginPage };
