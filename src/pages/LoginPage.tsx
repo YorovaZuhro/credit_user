7fc15a78b975f9fcc27322555d7a8c4f0a63f9a5
YozovaZuhro <z.yorzoda@gmail.com>
2024-03-28 07:20:45 -0700

    loginpage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,41 @@
-
+import React, { useState } from 'react'; 
 import { useNavigate } from 'react-router-dom'; 
 import "./LoginPage.css";
 
 function LoginPage() {
   const navigate = useNavigate(); 
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleClick = () => {
-    navigate('/request-page'); 
+  const handleClick = async () => {
+    try {
+      const response = await fetch('http://loans.iaomar.me/auth/sign-in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: login,
+          password: password
+        })
+      });
+
+      if (response.ok) {
+        navigate('/request-page'); 
+      } else {
+        console.error('Invalid email or password:', response.status);
+      }
+    } catch (error) {
+      console.error('Failed to sign-in:', error);
+    }
+  };
+
+  const handleLoginChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setLogin(event.target.value);
+  };
+
+  const handlePasswordChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setPassword(event.target.value);
   };
 
   return (
@@ -14,7 +43,7 @@ function LoginPage() {
       <div>
         <p>Вход в систему</p>
         <span>
-          Пожалуйста, введите ваш логин и пароль <br></br>ниже, чтобы войти в ваш аккаунт.
+          Пожалуйста, введите ваш логин и пароль <br />ниже, чтобы войти в ваш аккаунт.
         </span>
       </div>
       <div className="formcontainer">
@@ -26,15 +55,19 @@ function LoginPage() {
             className="input1"
             placeholder="ivanovivan@mail.com"
             required
+            value={login} 
+            onChange={handleLoginChange} 
           />
 
           <label className="label required">Пароль</label>
           <input
-            name="password" 
-            type="password" 
+            name="password"
+            type="password"
             className="input2"
             placeholder="*********"
             required
+            value={password} 
+            onChange={handlePasswordChange} 
           />
           <button type="button" className="submitbutton" onClick={handleClick}>
             Войти
